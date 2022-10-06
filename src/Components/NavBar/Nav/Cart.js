3cfd55ac0aa90dfe797b44d7453d3bb169e081f7
Diego Oliva <dieg81@gmail.com>
2022-10-06 17:02:43 -0300

    entrega

--- src/Components/NavBar/Nav/Cart.js
+++ src/Components/NavBar/Nav/Cart.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { context } from "../../../Context/CustomContext"
+import { Link } from "react-router-dom"
+ 
+
 
 export const Cart = () => {
+  
+  const { cart } = useContext(context)
+
+  if(cart.lenght === 0){
+    return (<h1>No agregaste productos , puedes ver <Link to={'/'} > aca </Link> </h1>)
+  }
+
+
   return (
-    <div>Cart</div>
+    
+    <>
+    {cart.map ((producto)=>{
+      return <h1 key={producto.id} >{producto.title} </h1>
+    })}
+    </>
   )
 }
 
